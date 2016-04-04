86df1e5bd720258e5b2c141be5eab8c431f4c7aa
wangzhengyi <wzyll1314@gmail.com>
2016-04-04 18:59:49 +0800

    完成PendingPostQueue的中文注解

--- src/main/java/org/greenrobot/eventbus/PendingPostQueue.java
+++ src/main/java/org/greenrobot/eventbus/PendingPostQueue.java
@@ -1,12 +1,13 @@
 package org.greenrobot.eventbus;
 
 /**
- * 通过head和tail指针维护一个PendingPost队列.
+ * 通过head和tail指针维护一个PendingPost的生产者-消费者模型队列.
  */
 final class PendingPostQueue {
     private PendingPost head;
     private PendingPost tail;
 
+    /** PendingPost入队,入队之后notifyAll. */
     synchronized void enqueue(PendingPost pendingPost) {
         if (pendingPost == null) {
             throw new NullPointerException("null cannot be enqueued");
@@ -23,6 +24,7 @@ final class PendingPostQueue {
         notifyAll();
     }
 
+    /** 取队头的PendingPost. */
     synchronized PendingPost poll() {
         PendingPost pendingPost = head;
         if (head != null) {
@@ -34,6 +36,7 @@ final class PendingPostQueue {
         return pendingPost;
     }
 
+    /** 取队头的PendingPost,如果此时队列为空,则让出对象锁等待maxMillisToWait后再取一次队列头部元素. */
     synchronized PendingPost poll(int maxMillisToWait) throws InterruptedException {
         if (head == null) {
             wait(maxMillisToWait);
